7b10e78c4c5cabdd290de3444b583688f3e5be08
ChatApp Developer <dev@chatapp.com>
2026-04-07 16:04:03 +0500

    Update

--- src/routes/channelroutes.js
+++ src/routes/channelroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const channelController = require('../controllers/ChannelController');
-const { validate } = require('../middlewares/Validate');
+const { validate, validateQuery } = require('../middlewares/Validate');
 const auth = require('../middlewares/auth/CheckTokenMiddleware');
 
 const channelExist = require('../middlewares/channel/CheckChannelExistMiddleware');
